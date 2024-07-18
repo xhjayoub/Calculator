84498f061d806fc6d948a6b0116a434f099e7bfb
Ayoub hj <medayoubhajamor197@gmail.com>
2024-07-18 10:48:18 +0100

    Add operate() to calculate 2 number

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function multiply(a, b) {
 function divide(a, b) {
     return a/b;
 }
+function operate(n1, op, n2) {
+    switch (op) {
+        case '+':
+            return add(n1,n2);
+        case '-':
+            return subtract(n1,n2);
+        case '*':
+            return multiply(n1,n2);
+        case '/':
+            return divide(n1,n2);
+    }
+}
+
+let num1;
+let op;
+let num2;
