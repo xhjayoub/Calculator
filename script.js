92e7d03c0d4e0dddfdd6eb9111b7f89e3b5de84e
Ayoub hj <medayoubhajamor197@gmail.com>
2024-07-18 11:11:03 +0100

    Display numbers

--- script.js
+++ script.js
@@ -22,7 +22,19 @@ function operate(n1, op, n2) {
             return divide(n1,n2);
     }
 }
+function display(n) {
+    const d = document.querySelector(".display span");
+    d.innerHTML += n;
+}
 
 let num1;
 let op;
 let num2;
+
+
+
+// Apply display to numbers
+const numArr = document.querySelectorAll(".nums button");
+numArr.forEach((e) => e.addEventListener("click", (e) => {
+    display(e.target.innerHTML);
+}));
